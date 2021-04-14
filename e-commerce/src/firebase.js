b242ef4f94426e608ae270e2309e52a4533d620d
tutr464b <tuangiangtum@gmail.com>
2021-04-14 22:36:14 +0200

    complete setup

---
+++ e-commerce/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDux4rhVbgn8LsOhP06qeeOxkRpfZBSt6Y",
+    authDomain: "doggie-741c7.firebaseapp.com",
+    projectId: "doggie-741c7",
+    storageBucket: "doggie-741c7.appspot.com",
+    messagingSenderId: "194408124176",
+    appId: "1:194408124176:web:0126e7855cb260a425df95",
+    measurementId: "G-0L8B0D0BGL"
+  };
